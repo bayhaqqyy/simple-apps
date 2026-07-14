c895316a0157803e998ca38fada3ce687c98fd8a
bayhaqqyy <114376645+bayhaqqyy@users.noreply.github.com>
2026-07-14 10:51:09 +0700

    feat: initialize express application with database connection and static file routing

--- apps/app.js
+++ apps/app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const app = express()
 const path = require('path')
 require('dotenv').config();
